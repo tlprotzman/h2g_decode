1ed8c0ce56bd34c3c06382dbd3b51eca506e720d
Tristan Protzman <tlprotzman@gmail.com>
2025-11-20 12:49:04 -0500

    remove unused code

--- src/waveform_builder.h
+++ src/waveform_builder.h
@@ -81,7 +81,6 @@ public:
     ~waveform_builder();
     bool build(std::list<sample*> *samples);
     bool build_v013(std::list<sample*> *samples);
-    bool build_v013_old(std::list<sample*> *samples);
     void unwrap_counters();
     std::list<kcu_event*>* get_complete() {return complete;}
 
